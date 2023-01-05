736dce8f98db571d25f729179e707cbde8db91e5
SandraConil <[sandra.conil@sncf.fr](mailto:sandra.conil@sncf.fr)>
2023-01-05 13:31:30 +0100

    fisrt commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// class person avec 2 attributs name et age
+var Person = /** @class */ (function () {
+    function Person(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    // mes 2 methodes de ma class Personn
+    Person.prototype.tellMyName = function () {
+        console.log("I am $(this.name)");
+    };
+    Person.prototype.tellMyAge = function () {
+        console.log("I am $(this.age) years old");
+    };
+    return Person;
+}());
+var person2 = new Person("John", 40);
+var person3 = new Person("Marie", 35);
+//2 nouvelles personnes
+person2.tellMyName();
+person2.tellMyAge();
+person3.tellMyAge();
+person3.tellMyName();
---
+++ index.ts
@@ -0,0 +1,30 @@
+// class person avec 2 attributs name et age
+
+class Person {
+    name: string;
+    age: number;
+
+    constructor(name:string, age:number){
+        this.name = name;
+        this.age = age;
+    }
+    
+// mes 2 methodes de ma class Personn
+    tellMyName(){
+        console.log(`I am $(this.name)`);
+    }
+
+    tellMyAge(){
+        console.log(`I am $(this.age) years old`);
+    }
+}
+
+const person2 = new Person ("John", 40);
+const person3 = new Person ("Marie", 35);
+
+//2 nouvelles personnes
+person2.tellMyName();
+person2.tellMyAge();
+
+person3.tellMyAge();
+person3.tellMyName();
